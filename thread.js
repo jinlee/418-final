d2810368a8f7a50c090a4fe5c6a3c3de03271c58
Jin Lee <jinlee26@gmail.com>
2014-04-18 22:26:42 -0400

    more robust auxiliary data passing

--- thread.js
+++ thread.js
@@ -5,114 +5,141 @@
 (function() {
 
     function Latch(next) {
-	this.ready = false;
-	this.next = next;
+        this.ready = false;
+        this.next = next;
+        this.required = [];
     }
 
     Latch.prototype.register = function (nextFunc) {
-	if (this.ready) {
-	    nextFunc();
-	    return;
-	}
-	this.next = nextFunc;
+        if (this.ready) {
+            nextFunc();
+            return;
+        }
+        this.next = nextFunc;
     }
 
     Latch.prototype.runNext = function () {
-	this.ready = true;
-	if (this.next !== undefined) {
-	    this.next();
-	}
+        this.ready = true;
+        if (this.next !== undefined) {
+            this.next();
+        }
     }
 
-    function Seq(arr, required) {
-	this.arr = arr === undefined ? [] : arr;
-	// TODO support later
-	this.required = required;
-	this.latch = new Latch();
-	this.latch.ready = true;
+    function Seq(arr) {
+        this.arr = arr === undefined ? [] : arr;
+        this.required = [];
+        this.latch = new Latch();
+        this.latch.ready = true;
     }
 
     /** internal helper functions */
     Seq.prototype.stringify = function (f) {
-	return 'onmessage = function (msg) {' +
-	       '    postMessage( (' + f.toString() + ')(msg.data) );' +
-	       '}';
-    }
+        var s = '';
+        for (var i = 0; i < this.required.length; i++) {
+            s += 'var ' + this.required[i].name + ';\n';
+        }
+        for (var i = 0; i < this.latch.required.length; i++) {
+            s += 'var ' + this.latch.required[i].name + ';\n';
+        }
 
-    Seq.prototype.stringifyWithRequired = function (f) {
-	var preStr = 'var ' + this.required.name + ';\n';
-	return 'onmessage = function (msg) {\n' +
-		    this.required.name + ' = msg.data.required;\n' +
-	       '    postMessage( (' + f.toString() + ')(msg.data.data) );\n' +
-	       '}';
+        s += 'onmessage = function (msg) {\n';
+
+        for (var i = 0; i < this.required.length; i++) {
+            s += this.required[i].name + ' = msg.data.required['+i+'].data;\n';
+        }
+        for (var i = 0; i < this.latch.required.length; i++) {
+            s += this.latch.required[i].name + ' = msg.data._required['+i+'].data;\n';
+        }
+        s += 'postMessage( (' + f.toString() + ')(msg.data.data) ); }';
+        return s;
     }
 
     Seq.prototype.createWorker = function(f) {
-	var worker;
-	var fString;
-	if (this.required === undefined) {
-	    fString = this.stringify(f);
-	} else {
-	    fString = this.stringifyWithRequired(f);
-	}
-	try {
-	    var blob = new Blob([fString], { type: 'text/javascript' });
-	    worker = new Worker(window.URL.createObjectURL(blob));
-	} catch (err) {
-	    throw err;
-	}
-	return worker;
+        var worker;
+        var fString;
+        fString = this.stringify(f);
+        try {
+            var blob = new Blob([fString], { type: 'text/javascript' });
+            worker = new Worker(window.URL.createObjectURL(blob));
+        } catch (err) {
+            throw err;
+        }
+        return worker;
     }
 
     Seq.prototype.runWorker = function (f, index, checkDone) {
-	var seq = this;
-	var worker = this.createWorker(f);
-	worker.onmessage = function (res) {
-	    worker.terminate();
-	    if (res.data !== undefined) {
-		seq.arr[index] = res.data;
-	    }
-	    checkDone();
-	};
-	// run the worker! GO GO GO!
-	if (this.required === undefined) {
-	    worker.postMessage(seq.arr[index]);
-	} else {
-	    worker.postMessage({ data: seq.arr[index],
-				 required: seq.required.data });
-	}
+        var seq = this;
+        var worker = this.createWorker(f);
+        worker.onmessage = function (res) {
+            worker.terminate();
+            if (res.data !== undefined) {
+                seq.arr[index] = res.data;
+            }
+            checkDone();
+        };
+        // run the worker! GO GO GO!
+        worker.postMessage({ data: seq.arr[index],
+                             required: seq.required,
+                             _required: seq.latch.required });
     }
 
+
     /** external functions */
+    Seq.prototype.require = function (req) {
+        this.required.push(req);
+    }
+
+    Seq.prototype.requireOnce = function (req) {
+        this.latch.required.push(req);
+    }
+
     Seq.prototype.map = function (f, cb) {
-	if (this.arr.length === 0) {
-	    if (cb !== undefined) {
-		cb(this.arr);
-	    }
-	    return this;
-	}
+        var that = this;
+        var numResponse = 0;
+        var newLatch = new Latch();
+        var checkDone = function() {
+            if (++numResponse === that.arr.length) {
+                if (cb !== undefined) {
+                    cb(that.arr);
+                }
+                newLatch.runNext();
+            }
+        }
 
-	var that = this;
-	var numResponse = 0;
-	var newLatch = new Latch();
-	var checkDone = function() {
-	    if (++numResponse === that.arr.length) {
-		if (cb !== undefined) {
-		    cb(that.arr);
-		}
-		newLatch.runNext();
-	    }
-	}
+        this.latch.register(function() {
+            if (that.arr.length === 0) {
+                if (cb !== undefined) {
+                    cb(that.arr);
+                }
+                newLatch.runNext();
+                return;
+            }
+            for (var i = 0; i < that.arr.length; i++) {
+                that.runWorker(f, i, checkDone);
+            }
+        });
+        this.latch = newLatch;
+        return this;
+    }
 
-	this.latch.register(function() {
-	    for (var i = 0; i < that.arr.length; i++) {
-		that.runWorker(f, i, checkDone);
-	    }
-	});
-	this.latch = newLatch;
-	return this;
+
+    /** internal helper functions for the parser */
+    function _() {
+    }
+
+    _.range = function (n) {
+        var r = new Array(n);
+        for (var i = 0; i < n; i++) {
+            r[i] = i;
+        }
+        return r;
+    }
+
+    _.makeSeq = function(arr, name) {
+        return new Seq(_.range(arr.length), { name: name, data: arr});
     }
 
     this.Seq = Seq;
+    this._ = _;
 
 })();
