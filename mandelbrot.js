101f4bddab7cce47ba10e9b0242425a98da1ae76
Jin Lee <jinlee26@gmail.com>
2014-05-07 20:00:33 -0400

    demo fixed

--- mandelbrot.js
+++ mandelbrot.js
@@ -8,42 +8,54 @@
         this.render = new Render(canvas);
         this.width = canvas.width;
         this.height = canvas.height;
-        this.max = 10000;
+        this.max = 5000;
     }
 
     Mandelbrot.prototype.runSequential = function() {
-        var start = new Date().getTime();
-        for (var x = 0; x < this.width; x++) {
-            for (var y = 0; y < this.height; y++) {
-                var x0 = ((x / this.width) * 3.5) - 2.5;
-                var y0 = ((y / this.height) * 2.0) - 1.0;
-                var re = x0;
-                var im = y0;
-                var i;
-                for (i = 0; i < this.max; i++) {
-                    if ((re*re) + (im*im) > 4.0) {
-                        break;
-                    }
-                    var n_re = re*re - im*im;
-                    var n_im = 2.0 * re * im;
-                    re = x0 + n_re;
-                    im = y0 + n_im;
+        var arr = _.range(this.width * this.height);
+        var width = this.width;
+        var height = this.height;
+        var max = this.max;
+        var single = function(xy) {
+            var x = xy % width;
+            var y = (xy / width) | 0;
+            var x0 = ((x / width) * 3.5) - 2.5;
+            var y0 = ((y / height) * 2.0) - 1.0;
+            var re = x0;
+            var im = y0;
+            var i;
+            for (i = 0; i < max; i++) {
+                if ((re * re) + (im*im) > 4.0) {
+                    break;
                 }
-                var res = ((i / this.max) * 255.0) | 0;
-                res = (res << 24) | (res << 16) | (res << 8) | 0xFF;
-                this.render.setPixel(x, y, res);
+                var n_re = re*re - im*im;
+                var n_im = 2.0 * re * im;
+                re = x0 + n_re;
+                im = y0 + n_im;
             }
+            var res = ((i / max) * 255.0) | 0;
+            return (res << 24) | (res << 16) | (res << 8) | 0xFF;
+        }
+        console.log("seq starting");
+        var start = new Date().getTime();
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = single(arr[i]);
         }
         var end = new Date().getTime();
         console.log("Sequential took : " + (end - start) + " ms");
-        this.render.render();
+        for (var i = 0; i < arr.length; i++) {
+            var x = i % this.width;
+            var y = (i / this.width) | 0;
+            this.render.setPixel(this.width - 1 - x, y, arr[i]);
+            this.render.render();
+        }
     }
 
     Mandelbrot.prototype.runParallel = function() {
         var arr = _.range(this.width * this.height);
         var single = function(xy) {
             var x = xy % width;
-            var y = xy / width;
+            var y = (xy / width) | 0;
             var x0 = ((x / width) * 3.5) - 2.5;
             var y0 = ((y / height) * 2.0) - 1.0;
             var re = x0;
@@ -66,17 +78,16 @@
         seq.require({ name: 'height', data: this.height});
         seq.require({ name: 'max', data: this.max});
         var start = new Date().getTime();
-        var end;
         var bleh = this;
         seq.map(single, function (res) {
-            end = new Date().getTime();
+            var end = new Date().getTime();
             console.log("Parallel took : " + (end - start) + " ms");
             for (var i = 0; i < res.length; i++) {
                 var x = i % bleh.width;
-                var y = i / bleh.width;
+                var y = (i / bleh.width) | 0;
                 bleh.render.setPixel(x, y, res[i]);
-                bleh.render.render();
             }
+            bleh.render.render();
         });
     }
 
